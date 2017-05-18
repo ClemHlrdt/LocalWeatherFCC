0a78cae7bfe857126fbe37b87c4d913d01866445
ClemHlrdt <clement.helardot@gmail.com>
2017-05-18 22:53:52 +0200

    FCC Forecast app almost done, animation to add

--- js/custom.js
+++ js/custom.js
@@ -2,19 +2,24 @@ var x = document.getElementById("location");
 
 $(document).ready(function() {
     getLocation();
+    //$('.jumbotron').addClass('hidden');
+    $('.jumbotron').hide();
 });
 var latitude, longitude;
-$('#location').click(getLocation); // click to call function getLocation
+//$('#location').click(getLocation); // click to call function getLocation
+
 
 /* Checks if location is enabled*/
+//Function getLocation
 function getLocation() {
     if (navigator.geolocation) {
         //if locatin is enabled, show position in button
         navigator.geolocation.getCurrentPosition(showPosition);
     } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
+        alert('Please enable location to get your forecast');
     }
 }
+//Function showPosition
 function showPosition(position) {
     /*x.innerHTML = "Latitude: " + position.coords.latitude +
     "<br>Longitude: " + position.coords.longitude;*/
@@ -28,14 +33,33 @@ function showPosition(position) {
     $.getJSON(url, getWeather, 'jsonp');
 }
 
+//Function getWeather
 var getWeather = function (data) {
+    $('.jumbotron').delay('slow').show('400', function() {
+        $('.element').addClass('animated pulse');
+    });
     var location = (data.location.name);
-    var tempC = (data.current.temp_c);
+     tempC = (data.current.temp_c);
+     tempCFull = tempC + "°C";
     var condition = (data.current.condition.text);
-    var path = "img/" + condition + ".svg";
+    var icon = (data.current.condition.icon).substr(29);
+    var link = "img/" + icon;
+
+    tempF = (tempC * 1.8 + 32) + "°F";
+    console.log(tempF);
     $('#city').text(location);
-    /*$('#city').text("City: " + location);*/
-    $('#temp').text("Current temperature: " + tempC + "°C");
+    $('#tempC').text(tempCFull );
+    $('#tempF').text(tempF).hide();
     $('#condition').text("Current condition: " + condition );
-    $('#icon').attr('src', path);
+    $('#icon').attr('src', link);
 };
+
+$('#tempC').click(function(event) {
+    $('#tempF').show().text(tempF);
+    $(this).hide();
+});
+
+$('#tempF').click(function(event) {
+    $('#tempC').show().text(tempCFull);
+    $(this).hide();
+});
